659ff73e9d3fcaabd3c4c71a1043fbe8f7c210e2
Manikandankb2403 <154958574+Manikandankb2403@users.noreply.github.com>
2025-06-13 14:37:17 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://college-complaint-management-system-99w0.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
