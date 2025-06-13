2d0b4227c2a3ff25ed6274caa5522b7b1247f8f4
Manikandankb2403 <154958574+Manikandankb2403@users.noreply.github.com>
2025-06-13 14:36:06 +0530

    Update RegisterStudent.jsx

--- frontend/src/pages/RegisterStudent.jsx
+++ frontend/src/pages/RegisterStudent.jsx
@@ -20,7 +20,7 @@ const RegisterStudent = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/register-student', {
+      await axios.post('https://college-complaint-management-system-99w0.onrender.com/api/auth/register-student', {
         ...formData,
         role: 'student',
         position: 'student',
