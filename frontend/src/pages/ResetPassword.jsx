781718dd3d4d9d54fbf3dc1b55a3bf8a44c30d09
Manikandankb2403 <154958574+Manikandankb2403@users.noreply.github.com>
2025-06-13 14:36:39 +0530

    Update ResetPassword.jsx

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -27,7 +27,7 @@ const ResetPassword = () => {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/reset-password', {
+      const res = await axios.post('https://college-complaint-management-system-99w0.onrender.com/api/auth/reset-password', {
         token,
         password: formData.password,
       });
